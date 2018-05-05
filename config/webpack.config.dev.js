d70d170fbbf90ef22d83b2040db0f435d9698bb7
Patrick Roza <contact@patrickroza.com>
2018-05-05 12:08:51 +0200

    Add missing tsx? exclude

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -355,7 +355,7 @@ module.exports = {
             // its runtime that would otherwise be processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(tsx?)$/, /\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
