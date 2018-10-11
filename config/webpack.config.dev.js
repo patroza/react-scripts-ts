6f945e44d6b965eb8de726e57fc81338795f0a34
Patrick Roza <contact@patrickroza.com>
2018-10-11 16:40:47 +0200

    Enable svgr icon mode

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -227,6 +227,7 @@ module.exports = {
             use: ({resource}) => ({
               loader: require.resolve('@svgr/webpack'),
               options: {
+                icon: true,
                 svgoConfig: {
                   plugins: [{
                     "cleanupIDs": {
